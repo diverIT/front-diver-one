f7b9d21185e04662cc0927d21ac01e5a2b5a2ace
richard-allcca <richard-allcca-llano@hotmail.com>
2022-01-26 18:53:55 -0500

    Start proyect

---
+++ pages/aside-content/Aside.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Aside.module.css";
+
+const Aside = () => {
+  return <div className={styles.title}>Soy el Aside</div>;
+};
+
+export default Aside;
---
+++ pages/main-content/MainContent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Main.module.css";
+
+const Main = () => {
+  return <div className={styles.mainContent}>Soy el Main</div>;
+};
+
+export default Main;
