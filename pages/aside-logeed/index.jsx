3c615f49c3895bea6a5abfedfe9b4d0b5d292ff3
richard-allcca <richard-allcca-llano@hotmail.com>
2022-02-03 16:54:59 -0500

    update styles aside

--- pages/aside-logeed/index.jsx
+++ pages/aside-logeed/index.jsx
@@ -1,3 +1,16 @@
+import {
+  faCogs,
+  faHome,
+  faProjectDiagram,
+  faTasks,
+  faUser,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import styles from "../../styles/Aside.module.css";
+import Button from "../components/Button";
+
 export default function Aside() {
   // const location = useLocation().pathname;
   function handleClick() {
@@ -5,78 +18,55 @@ export default function Aside() {
   }
 
   return (
-    <aside className="aside-box">
-      <h2>Este el el sitio logeado</h2>
-      {/* <div className="box-1">
-        <Link to="/home">
-          <div className={location === "/home" ? "link-active" : "link"}>
-            <FontAwesomeIcon icon={faHome} className="icon" />
-            Inicio
+    <>
+      <aside className={styles.contentLogeed}>
+        <section className={styles.box}>
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faHome} className={styles.icon} />
+            <Link href="/">Inicio</Link>
           </div>
-        </Link>
-        <Link to="/profile">
-          <div className={location === "/profile" ? "link-active" : "link"}>
-            <FontAwesomeIcon icon={faUser} className="icon" />
-            Perfil
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faUser} className={styles.icon} />
+            <Link href="/perfil">Perfil</Link>
           </div>
-        </Link>
-      </div>
-      <div className="box-2">
-        <Link to="/activeProjects">
-          <div
-            className={location === "/activeProjects" ? "link-active" : "link"}
-          >
-            <FontAwesomeIcon icon={faProjectDiagram} className="icon" />
-            Proyectos Activos
+        </section>
+
+        <section className={styles.box}>
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faProjectDiagram} className={styles.icon} />
+            <Link href="/activeProjects">Proyectos Activos</Link>
           </div>
-        </Link>
-        <Link to="/teams">
-          <div className={location === "/teams" ? "link-active" : "link"}>
-            <FontAwesomeIcon icon={faUsers} className="icon" />
-            Equipos
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faUsers} className={styles.icon} />
+            <Link href="/teams">Equipos</Link>
           </div>
-        </Link>
-      </div>
-      <div className="box-3">
-        <div className="box-children1">
-          <Link to="/finishedprojects">
-            <div
-              className={
-                location === "/finishedprojects" ? "link-active" : "link"
-              }
-            >
-              <FontAwesomeIcon icon={faProjectDiagram} className="icon" />
-              Proyectos Listos
-            </div>
-          </Link>
-          <Link to="/tasks">
-            <div className={location === "/tasks" ? "link-active" : "link"}>
-              <FontAwesomeIcon icon={faTasks} className="icon" />
-              Tareas
-            </div>
-          </Link>
-        </div>
-        <div className="box-children2">
-          <Button
-            value={"Crear Proyecto "}
-            onClick={handleClick}
-            className={"button-newproject"}
-          />
-          <div>
-            <Link to="/" style={{ textDecoration: "underline" }}>
-              Cerrar Sesion
-            </Link>
+        </section>
+
+        <section className={styles.box}>
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faProjectDiagram} className={styles.icon} />
+            <Link href="/finishedprojects">Proyectos Listos</Link>
           </div>
-        </div>
-      </div>
-      <div className="box-4">
-        <Link to="#">
-          <div>
-            <FontAwesomeIcon icon={faCogs} className="icon" />
-            Soporte
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faTasks} className={styles.icon} />
+            <Link href={"/tasks"}>Tareas</Link>
           </div>
-        </Link>
-      </div> */}
-    </aside>
+        </section>
+        <section className={styles.boxNavFooter}>
+          <div className={styles.boxNav}>
+            <Button
+              message="Iniciar Proyecto"
+              background="yellow"
+              width="40%"
+            />
+            <Link href="/">Cerrar Sesion</Link>
+          </div>
+          <div className={styles.boxNav}>
+            <FontAwesomeIcon icon={faCogs} className={styles.icon} />
+            <Link href="#">Soporte</Link>
+          </div>
+        </section>
+      </aside>
+    </>
   );
 }
--- pages/components/Button.jsx
+++ pages/components/Button.jsx
@@ -1,13 +1,36 @@
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styles from "../../styles/Main.module.css";
-function Button() {
+// import styles from "../../styles/Main.module.css";
+function Button({ message, background, width }) {
   return (
     <>
-      <button className={styles.button}>
-        <FontAwesomeIcon icon={faUser} className={styles.buttonLogin} />
-        Iniciar Sesion
+      <button>
+        <span className="icon">
+        <FontAwesomeIcon icon={faUser} />
+        </span>
+        {message}
       </button>
+      <style jsx>{`
+        button {
+          min-width: ${width};
+          height: 3rem;
+          background-color: ${background};
+          border-radius: 20px;
+          border: none;
+          padding: 0.9rem;
+          color: black;
+          left: 0;
+          font-size: .8rem;
+          display: flex;
+          justify-content: space-between;
+        }
+        .icon {
+          display: inline-block;
+          width: 14px;
+          color: black;
+          margin-right: 5px;
+        }
+      `}</style>
     </>
   );
 }
